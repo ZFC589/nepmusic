5981a7974de861fe34e27e7d5c7298f4182dae9c
ZFC589 <74024452+ZFC589@users.noreply.github.com>
2022-02-24 22:06:15 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ const { Player } = require('discord-player');
 const { Client, Intents } = require('discord.js');
 
 global.client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ],
     disableMentions: 'everyone',
 });
 
